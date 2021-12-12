5e03d7dcd74c5f85538ad9c145f72bdfc8ae3c4f
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2021-12-12 21:37:13 +0100

    feat(Backend): Add note.controller

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,7 +1,8 @@
 import { Controller } from '../types';
 import AuthorizationController from './authorization/authorization.controller';
 import TaskController from './task/task.controller';
+import NoteController from './note/note.controller';
 
-const controllers: Controller[] = [new AuthorizationController(), new TaskController()];
+const controllers: Controller[] = [new AuthorizationController(), new TaskController(), new NoteController()];
 
 export default controllers;
