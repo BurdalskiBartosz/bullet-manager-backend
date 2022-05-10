b1761fdb1eccdba83936fdc5c7c2115841fdeb4d
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-05-10 03:05:12 +0200

    Remove old controllers

--- controllers/index.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '../types';
-import AuthorizationController from './authorization/authorization.controller';
-import TaskController from './task/task.controller';
-
-const controllers: Controller[] = [new AuthorizationController(), new TaskController()];
-
-export default controllers;
