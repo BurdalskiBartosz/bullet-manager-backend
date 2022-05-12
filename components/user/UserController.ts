e98e3cd984b4072b8fb727423b9f7fc342f6f712
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-05-13 01:44:19 +0200

    Add logout route and login logic with setting cookie

--- components/user/UserController.ts
+++ components/user/UserController.ts
@@ -9,22 +9,29 @@ class UserController extends Controller {
 	initializeRoutes() {
 		this.router.post(`${this.path}/login`, async(this.login));
 		this.router.post(`${this.path}/register`, async(this.register));
+		this.router.post(`${this.path}/logout`, async(this.logout));
 	}
 
 	private login = async (req: tRequestWithUser, res: Response, next: NextFunction) => {
 		const data = this.getData(req);
-		const userToken = await this.service.login(data);
-		res.cookie('token', userToken, {
-			maxAge: 864000000,
+		const { user, token } = await this.service.login(data);
+
+		res.cookie('token', token, {
+			maxAge: 24 * 60 * 60 * 1000 * 10,
 			httpOnly: true,
 			secure: false
 		});
-		res.sendStatus(200);
+		res.send({ user }).status(200);
 	};
 
 	private register = async (req: Request, res: Response, next: NextFunction) => {
 		await this.service.register(req);
 		res.sendStatus(201);
+	};
+
+	private logout = (req: Request, res: Response, next: NextFunction) => {
+		res.clearCookie('token');
+		res.sendStatus(200);
 	};
 }
 
--- components/user/UserService.ts
+++ components/user/UserService.ts
@@ -20,11 +20,18 @@ class UserService implements Service {
 			}
 		});
 
-		if (!user) throw new HttpException('auterrror', 404, 'NOT_FOUND_USER_WITH_GIVEN_DATA');
+		if (!user) throw new HttpException(404, 'NOT_FOUND_USER_WITH_GIVEN_DATA');
+
 		const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
-		if (!isPasswordValid) throw new HttpException('auterrror', 404, 'NOT_FOUND_USER_WITH_GIVEN_DATA');
+		if (!isPasswordValid) throw new HttpException(404, 'NOT_FOUND_USER_WITH_GIVEN_DATA');
+
 		const tokenService = new TokenService();
-		return tokenService.create(user.id);
+		const token = await tokenService.create(user.id);
+		const { password, ...userData } = user;
+		return {
+			user: userData,
+			token
+		};
 	}
 
 	async register(req: Request) {
