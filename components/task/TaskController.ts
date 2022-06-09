7d6157bf0a8e802cbcb1080043fa2882054825f3
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-06-09 03:46:46 +0200

    Add base routes in TaskController and methods in TaskService

--- components/task/TaskController.ts
+++ components/task/TaskController.ts
@@ -5,7 +5,23 @@ import { Controller } from '../shared';
 class TaskController extends Controller {
 	public path: string = '/task';
 
-	initializeRoutes() {}
+	initializeRoutes() {
+		this.router.get(`${this.path}/:id`, async(this.getTask));
+		this.router.get(`${this.path}`, async(this.getTasks));
+		this.router.post(`${this.path}`, async(this.createTask));
+		this.router.patch(`${this.path}/:id`, async(this.editTask));
+		this.router.delete(`${this.path}/:id`, async(this.deleteTask));
+	}
+
+	private getTask = async () => {};
+
+	private getTasks = async () => {};
+
+	private createTask = async () => {};
+
+	private editTask = async () => {};
+
+	private deleteTask = async () => {};
 }
 
 export default TaskController;
--- components/task/TaskService.ts
+++ components/task/TaskService.ts
@@ -1,5 +1,15 @@
 import { Service } from '../shared';
 
-class TaskService implements Service {}
+class TaskService implements Service {
+	async getTask() {}
+
+	async getTasks() {}
+
+	async createTask() {}
+
+	async editTask() {}
+
+	async deleteTask() {}
+}
 
 export default TaskService;
