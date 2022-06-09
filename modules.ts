4724c2e25ca267fffdbcaca1fe1020abdb235c01
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-06-09 23:23:15 +0200

    Import and add to array new Modules in modules.ts

--- modules.ts
+++ modules.ts
@@ -1,4 +1,7 @@
 import UserModule from './components/user/UserModule';
 import TaskModule from './components/task/TaskModule';
+import CommentModule from './components/Comment/CommentModule';
+import ActivityModule from './components/Activity/ActivityModule';
+import TagModule from './components/Tag/TagModule';
 
-export const modules = [UserModule, TaskModule];
+export const modules = [UserModule, TaskModule, CommentModule, ActivityModule, TagModule];
