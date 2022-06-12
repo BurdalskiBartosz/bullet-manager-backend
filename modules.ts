abc66e84267e57ed5ea0a71800b2e2333ec2398e
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-06-12 03:03:12 +0200

    Import TokenModule to modules.ts

--- modules.ts
+++ modules.ts
@@ -3,5 +3,13 @@ import TaskModule from './components/Task/TaskModule';
 import CommentModule from './components/Comment/CommentModule';
 import ActivityModule from './components/Activity/ActivityModule';
 import TagModule from './components/Tag/TagModule';
+import TokenModule from './components/Token/TokenModule';
 
-export const modules = [UserModule, TaskModule, CommentModule, ActivityModule, TagModule];
+export const modules = [
+	UserModule,
+	TokenModule,
+	TaskModule,
+	CommentModule,
+	ActivityModule,
+	TagModule
+];
