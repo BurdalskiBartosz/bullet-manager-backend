5402762ad00cc0ebf1365501670d5a7d3cf710d2
Bartosz Burdalski <bartosz.burdalski@gmail.com>
2022-07-08 00:02:22 +0200

    Add AuthModule

---
+++ components/Auth/AuthController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { async } from '../../helpers';
+import { Controller, tLoginData, tRegistrationData } from '../shared';
+
+class AuthController extends Controller {
+	public path: string = '/auth';
+
+	initializeRoutes() {
+		this.router.post(`${this.path}/login`, async(this.login));
+		this.router.post(`${this.path}/register`, async(this.register));
+		this.router.post(`${this.path}/logout`, async(this.logout));
+	}
+
+	private login = async (req: Request, res: Response) => {
+		const data: tLoginData = req.body;
+		const { user, token } = await this.service.login(data);
+
+		res.cookie('token', token, {
+			maxAge: 24 * 60 * 60 * 1000 * 10,
+			httpOnly: true,
+			secure: false
+		});
+		res.send({ user }).status(200);
+	};
+
+	private register = async (req: Request, res: Response) => {
+		const data: tRegistrationData = req.body;
+		await this.service.register(data);
+		res.sendStatus(201);
+	};
+
+	private logout = (req: Request, res: Response) => {
+		res.clearCookie('token');
+		res.sendStatus(200);
+	};
+}
+
+export default AuthController;
---
+++ components/Auth/AuthService.ts
@@ -0,0 +1,49 @@
+import HttpException from '../../exceptions/httpException';
+import prisma from '../../prisma/prismaClient';
+import * as bcrypt from 'bcrypt';
+import { Service, tLoginData, tRegistrationData } from '../shared';
+import TokenService from '../token/TokenServices';
+
+class AuthService implements Service {
+	async login(data: tLoginData) {
+		const user = await prisma.user.findFirst({
+			where: {
+				OR: [
+					{
+						login: data.loginOrEmail
+					},
+					{
+						email: data.loginOrEmail
+					}
+				]
+			}
+		});
+
+		if (!user) throw new HttpException(404, 'Not found user with given data');
+
+		const isPasswordValid = await bcrypt.compare(data.password, user.password);
+		if (!isPasswordValid) throw new HttpException(404, 'Not found user with given data');
+
+		const tokenService = new TokenService();
+		const token = await tokenService.create(user.id);
+		const { password, ...userData } = user;
+		return {
+			user: userData,
+			token
+		};
+	}
+
+	async register(data: tRegistrationData) {
+		const salt = await bcrypt.genSalt();
+		const hashedPassword = await bcrypt.hash(data.password, salt);
+		await prisma.user.create({
+			data: {
+				login: data.login,
+				email: data.email,
+				password: hashedPassword
+			}
+		});
+	}
+}
+
+export default AuthService;
